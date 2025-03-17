b34d14aceaf24b9975ea7636ce53c8ecfd19b6db
Kalicz Péter <kaliczp@gmail.com>
2025-03-17 10:18:00 +0100

    Initial R script is added

---
+++ Intro.R
@@ -0,0 +1,32 @@
+## Creat an empty folder!
+## Change in R the working directory
+## Check it!
+getwd()
+help(getwd)
+WD
+a <- 1
+letters
+LETTERS
+LETTERS[2]
+search()
+d <- c("A","B")
+e <- c(1,2,3)
+1:10
+letters[2:5]
+ls()
+?diag
+diag(10)
+Mt <- diag(10)
+ls()
+Mt
+Mt[1,1]
+Mt[1,3]
+as.character(1)
+as.data.frame(Mt)
+Mt.df <- as.data.frame(Mt)
+ls()
+str(c)
+str(e)
+str(Mt)
+str(Mt.df)
+Mt.df$V1
