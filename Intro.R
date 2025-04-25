ac61c4ca1cbf985acb6d77e0abdfbbd13a2b7e3e
Kalicz Péter <kaliczp@gmail.com>
2025-04-25 12:19:33 +0200

    Generated random dataset based on regression

--- Intro.R
+++ Intro.R
@@ -92,3 +92,7 @@ summary(Model) # See summary
 plot(Model) # graphical tests
 
 ## Generate artifical points
+Gener.df <- data.frame(Na = sample(2:500, 50)) # Generate 50 random points between 2 and 500
+Gener.df$SAR <- Gener.df$Na * 0.2482 + 22 + rnorm(50, sd = 10) # Generate pairs based on regression
+plot(SAR ~ Na, data = Gener.df, xlim = c(0,500), ylim = c(0,170)) # Plot generated data
+points(SAR ~ Na, data = SoilOK.df, col = "red", lwd = 3) # Put original point on it
