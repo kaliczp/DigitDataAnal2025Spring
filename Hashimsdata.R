2b602c385c2e2e666f065e9882421e2d790b67d7
Kalicz Péter <kaliczp@gmail.com>
2025-05-23 11:47:04 +0200

    Convert to factor species type

--- Hashimsdata.R
+++ Hashimsdata.R
@@ -7,3 +7,4 @@ WadElbashir <- as.data.frame(read_excel("Wad Elbashir.xlsx"))
 ## Look at dataset
 str(WadElbashir)
 summary(WadElbashir)
+WadElbashir[, "Species Type"] <- factor(WadElbashir[, "Species Type"])
